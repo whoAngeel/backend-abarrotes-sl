1afa2d87a815805f30ec21e136ae26c5445792c3
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-09-12 23:25:17 -0600

    Iniciando la app de express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hola mundo servidor");
+const express = require('express');
+const debug = require('debug')("api:index");
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express()
+
+// MIDLEWARES
+app.use(cors());
+app.use(morgan("dev"))
+
+app.get('', (req, res) => {
+    res.send("Hola mundo");
+})
+
+app.listen(3000, () => {
+    debug("Servidor ejecutandose en http://localhost:3000")
+})
