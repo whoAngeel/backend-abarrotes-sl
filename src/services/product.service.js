ea60f6f4cb6b22f48972c5e124dbb522412ca114
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-13 23:57:46 -0600

    feat(product.service): creando los metodos del servicio
    solo las funciones del servicio de productos

---
+++ src/services/product.service.js
@@ -0,0 +1,24 @@
+class ProductsService {
+
+    constructor() {
+
+    }
+
+    create() {
+
+    }
+    update(id, data) {
+
+    }
+    delete(id) {
+
+    }
+    find(id) {
+
+    }
+    findAll() {
+
+    }
+}
+
+module.exports = ProductsService
