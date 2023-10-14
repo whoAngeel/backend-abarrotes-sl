49e27783807a4fdc3d642f80c3503913dd0f159b
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-14 12:40:23 -0600

    feat(lib/sequelize): creando una conexion a la bd con sequelize

--- src/libs/postgres.pool.js
+++ src/libs/postgres.pool.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
 
-const { config } = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser)
-const PASS = encodeURIComponent(config.dbPass)
-const URI = `postgres://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const pool = new Pool({
     connectionString: URI
---
+++ src/libs/sequelize.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser)
+const PASS = encodeURIComponent(config.dbPass)
+const URI = `postgres://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
+const sequelize = new Sequelize(URI, {
+    dialect: 'postgres',
+    logging: true,
+
+})
+
+module.exports = sequelize
