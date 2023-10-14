4616c93f888b1cb60e0c5e8fb742d4a665816b19
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-14 12:36:25 -0600

    feat(config): configurando las variables de entorno para hacerlas accesibles

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const config = {
+    env: process.env.NODE || 'dev',
+    port: process.env.PORT || 3000,
+    dbUser: process.env.DB_USER,
+    dbPass: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    dbHost: process.env.DB_HOST
+}
+
+module.exports = { config }
