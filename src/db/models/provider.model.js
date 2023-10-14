ca3589784f64dd1d585d607f0e40f9336b06353d
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-14 16:48:25 -0600

    feat(provider.model): modelo de proveedores

---
+++ src/db/models/provider.model.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes, Sequelize } = require("sequelize");
+
+const PROVIDER_TABLE = 'providers'
+
+const ProviderSchame = {
+    id: {
+        type: DataTypes.INTEGER(10).UNSIGNED,
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+    },
+    phone: {
+        type: DataTypes.STRING
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    }
+}
+
+class Provider extends Model {
+    static associate(models) { }
+
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: PROVIDER_TABLE,
+            modelName: "Provider",
+            timestamps: false
+        }
+    }
+}
+
+module.exports = {
+    ProviderSchame,
+    PROVIDER_TABLE,
+    Provider
+}
