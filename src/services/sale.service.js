0850b9e9d07592796913ccbd5362611622c0de71
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-18 01:10:11 -0600

    feat(sale.service): servicio de ventas

---
+++ src/services/sale.service.js
@@ -0,0 +1,33 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class SalesService {
+    constructor() { }
+
+    async find() { // TODO: cambiar
+        return await models.Sale.findAll()
+    }
+
+    async create(data) {
+        const newSale = await models.Sale.create(data)
+        return newSale
+    }
+    async findOne(id) {
+        const sale = await models.Sale.findByPk(id, {
+            include: [{
+                association: 'employee',
+                include: ['user']
+            }],
+        }, 'items')
+        if (!sale) throw boom.notFound("Venta no encontrada")
+        return sale
+    }
+
+    async update(id, changes) {// no se va a poder actualizar una venta
+        return { id, changes }
+    }
+
+    async delete(id) {// no se va a poder eliminar una venta
+        return { id }
+    }
+}
