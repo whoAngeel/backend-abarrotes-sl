9053c03b7c1fe67ac619b87e8b2fb9a0e77cb18c
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-10-28 19:37:40 -0600

    Swagger v2

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const morgan = require('morgan');
 //swagger
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerSpec = require('./swaggerSpec');
+// const swaggerSpec = require('./swaggerSpec');
+const { swaggerDocs: V1SwaggerDocs } = require('./swaggerSpec');
+
 
 const routerApi = require('./routes')
 const { errorHandler, logErrors, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 const { checkApiKey } = require('./middlewares/auth.handler');
+const { config } = require('./config/config');
 
 const app = express()
 
@@ -18,7 +21,7 @@ app.use(cors());
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use("/api/doc", swaggerUI.serve, swaggerUI.setup(swaggerJsdoc(swaggerSpec))) // endpoint  para mostrar la documentacion
+// app.use("/api/doc", swaggerUI.serve, swaggerUI.setup(swaggerJsdoc(swaggerSpec))) // endpoint  para mostrar la documentacion
 
 require('./utils/auth'); // usar las strategias de passport
 // ROUTES
@@ -35,5 +38,6 @@ app.use(ormErrorHandler)
 app.use(errorHandler)
 
 app.listen(3000, () => {
-    debug("Servidor ejecutandose en http://localhost:3000")
+    debug(`Servidor ejecutandose en http://localhost:${config.port}`);
+    V1SwaggerDocs(app, config.port)
 })
--- src/swaggerSpec.js
+++ src/swaggerSpec.js
@@ -1,17 +1,41 @@
 const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const { config } = require('./config/config');
+const debug = require('debug')('api:swaggerSpec');
 
-module.exports = {
+const options = {
     definition: {
         openapi: "3.0.0",
         info: {
             title: "API Rest para la tienda de abarrotes sanluis",
             version: "1.0.0"
         },
-        servers: [
-            {
-                url: "http://localhost:3000"
-            }
-        ]
+        // servers: [
+        //     {
+        //         url: "http://localhost:3000"
+        //     }
+        // ]
     },
-    apis: [`${path.join(__dirname, './routes/*.js')}`]
+    apis: [`src/routes/index.js`,
+        'src/routes/auth.routes.js',
+        'src/routes/categories.routes.js', 'src/routes/employees.routes.js', 'src/routes/products.routes.js', 'src/routes/sales.routes.js', 'src/routes/users.routes.js']
 }
+
+const swaggerSpec = swaggerJsdoc(options);
+
+const swaggerDocs = (app, port) => {
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.get('/api/docs.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec)
+    })
+
+    debug(`📃Docs available at http://localhost:${config.port}/api/docs`)
+}
+
+module.exports = {
+    swaggerDocs
+}
+
+/// docs en json format
