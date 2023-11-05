ef9c5206e37eda58436bec8705152b6b7f9a2940
Ubish <solzptk@gmail.com>
2023-11-04 22:43:54 -0600

    fix(reports): correcion de ruta y datos necesarios para la misma

--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
-const validatorHandler = require('../middlewares/validator.handler');Router
+const validatorHandler = require('../middlewares/validator.handler');
+const { queryReportSchema } = require('../schemas/reports.schema');
 const { checkRoles } = require("../middlewares/auth.handler");
 const passport = require("passport");
 
@@ -8,7 +9,7 @@ const router = Router()
 router.get('/',
     passport.authenticate('jwt', { session: false }),
     checkRoles('admin'),
-    validatorHandler(queryProductSchema, 'body'), async (req, res, next) => {
+    validatorHandler(queryReportSchema, 'body'), async (req, res, next) => {
     try {
         res.status(201).json("Oa")
     } catch (error) {
--- src/schemas/reports.schema.js
+++ src/schemas/reports.schema.js
@@ -1,5 +1,14 @@
 const joi = require('joi');
 
+const limit = joi.number().integer();
+const offset = joi.number().integer();
+const createdAt = joi.string().isoDate();
+
+const queryReportSchema = joi.object({
+    offset,
+    limit,
+});
+
 module.exports = {
-    updateStockProductSchema
+    queryReportSchema
 };
